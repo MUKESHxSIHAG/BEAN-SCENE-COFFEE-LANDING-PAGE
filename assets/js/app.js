74ff68775d31c77454f6cd321510f63931a8551d
mukeshhr22 <msihag028@gmail.com>
2023-07-03 13:47:55 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+ $(".move_sl").slick({
+            slidesToShow: 1,
+            infinite: true,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrow: true,
+
+
+            // prevArrow: $(".previous"),
+            // nextArrow: $(".Next_Btn"),
+        });
